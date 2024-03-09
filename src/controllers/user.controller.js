8eb7522d14384badd45e0e4ffd16a6c756a7f7a9
nischay-chauhan <masternischay@outlook.com>
2024-03-09 22:08:45 +0530

    Logout + auth Middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -130,5 +130,29 @@ const loginUser = asyncHandler(async (req, res) => {
       );
   });
   
+  const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $unset: {
+          refreshToken: 1,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+  
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+  
+    return res
+      .status(200)
+      .clearCookie("accessToken", options)
+      .clearCookie("refreshToken", options)
+      .json(new ApiResponse(200, {}, "User logged Out"));
+  });
 
-export {registerUser , loginUser}
+export {registerUser , loginUser , logoutUser}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,12 @@
 import {  Router } from "express";
 import { upload } from "../middleware/multer.middleware.js";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(upload.single("avatar") , registerUser);
 router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT ,logoutUser)
 
 export default router;
