ac7424320005077adcbbcc66503e88269446e97a
nischay-chauhan <masternischay@outlook.com>
2024-03-10 09:47:23 +0530

    Added Specific Post by a User

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -10,14 +10,50 @@ const createPost = asyncHandler(async(req , res) => {
     if(!content.trim()){
         throw new ApiError(400 , "Post Content is Required");
     }
+
     const newPost = await Post.create({
         content ,
         user: req.user.id
     })
+
+    const isPostCreatd = await Post.findById(newPost._id)
+
+    if(!isPostCreatd){
+        throw new ApiError(500 , "Something went wrong while creating post")
+    }
 
     return res
     .status(201)
     .json(new ApiResponse(200 , newPost , "Post Created SuccessFully"))
 })
 
-export {createPost}
+
+const getSpecificUserPosts = asyncHandler(async(req , res) => {
+    const userId = req.params.id;
+    if(!userId){
+        throw new ApiError(400 , "Invalid user Id or User Id is Required");
+    }
+
+    let { page = 1, pageSize = 10 } = req.query;
+    page = parseInt(page);
+    pageSize = parseInt(pageSize);
+
+    const skip = (page - 1) * pageSize;
+
+    const posts = await Post.find({ user: userId })
+                             .populate({
+                                 path: 'user',
+                                 select: '-password -refreshToken'
+                             })
+                             .skip(skip)
+                             .limit(pageSize);
+
+    if(!posts){
+        throw new ApiError(500 , "Something went wrong while fetching posts");
+    }
+
+    return res.status(200).json(new ApiResponse(200 , posts , "Posts fetched successfully"));
+});
+
+
+export {createPost , getSpecificUserPosts}
--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/auth.middleware.js";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, getSpecificUserPosts } from "../controllers/post.controller.js";
 
 const router = Router()
 
 router.route("/content").post(verifyJWT , createPost);
+router.route("/:id/posts").get(verifyJWT , getSpecificUserPosts)
 
 export default router
